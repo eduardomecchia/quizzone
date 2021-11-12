a4f011e9f7ae33db6fe6b1954228c3d4777820a4
Eduardo Mecchia <eduardo.mecchia@accenture.com>
2021-11-12 15:25:51 +0100

    Delete HelloWorld component and add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/new">Add new page</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+  nav {
+    background-color: red;
+  }
+</style>
