3c521aef76761e08339546ba1bdd91fe72bf3b07
Eduardo Mecchia <eduardo.mecchia@accenture.com>
2021-11-12 16:07:07 +0100

    Convert navbar to Bootstrap 5 version

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,17 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <router-link to="/">Home</router-link>
-        <router-link to="/new">Add new page</router-link>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Quizzone</a>
+
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+        <div class="navbar-nav text-white">
+          <router-link to="/">Home</router-link>
+          <router-link to="/new" class="ms-3">Add new page</router-link>
+        </div>
       </div>
     </div>
   </nav>
@@ -20,7 +24,7 @@ export default {
 </script>
 
 <style scoped>
-  nav {
-    background-color: red;
+  a {
+    color: #fff;
   }
 </style>
