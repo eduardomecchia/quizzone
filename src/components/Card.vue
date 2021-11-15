7d1b824abf871823b5c07d3e048722ad67b2fe85
Eduardo Mecchia <eduardo.mecchia@accenture.com>
2021-11-15 11:54:33 +0100

    Add labels to card fields

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,11 +5,30 @@
         </div>
 
         <div class="card-body">
-            <div class="name">{{ getUser.name }}</div>
-            <div class="surname">{{ getUser.surname }}</div>
-            <div class="email">{{ getUser.email }}</div>
-            <div class="city">{{ getUser.city }}</div>
-            <div class="notes">{{ getUser.notes }}</div>
+            <div class="d-flex align-items-center">
+                <span class="label fw-bold me-2">Name:</span>
+                <div class="name">{{ getUser.name }}</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <span class="label fw-bold me-2">Surname:</span>
+                <div class="surname">{{ getUser.surname }}</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <span class="label fw-bold me-2">E-mail:</span>
+                <div class="email">{{ getUser.email }}</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <span class="label fw-bold me-2">City:</span>
+                <div class="city">{{ getUser.city }}</div>
+            </div>
+
+            <div class="d-flex align-items-center">
+                <span class="label fw-bold me-2">Notes:</span>
+                <div class="notes">{{ getUser.notes }}</div>
+            </div>
         </div>
 
         <router-link :to="{ name: 'User', params: { id: getUser.id } }" class="btn btn-primary">
